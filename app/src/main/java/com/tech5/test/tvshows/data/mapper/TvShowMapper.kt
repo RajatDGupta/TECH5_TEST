647c8cc6c5a450d014122a27cc75e99b0e82c4eb
Neeraj <ngupta.gupta53@gmail.com>
2026-06-24 14:06:48 +0530

    Implement TV show details mapper
    - Add `toTvShowDetails` extension function to `TvShowMapper.kt` to convert `TVShowDetailDTO` to the `TvShowDetails` domain model.
    - Include logic to prepend the TMDB image base URL to backdrop and poster paths.

--- app/src/main/java/com/tech5/test/tvshows/data/mapper/TvShowMapper.kt
+++ app/src/main/java/com/tech5/test/tvshows/data/mapper/TvShowMapper.kt
@@ -1,7 +1,9 @@
 package com.tech5.test.tvshows.data.mapper
 
 import com.tech5.test.tvshows.data.remote.dto.TVShowDTO
+import com.tech5.test.tvshows.data.remote.dto.details.TVShowDetailDTO
 import com.tech5.test.tvshows.domain.model.TvShow
+import com.tech5.test.tvshows.domain.model.TvShowDetails
 
 fun TVShowDTO.toTvShow(): TvShow {
     return TvShow(
@@ -11,5 +13,19 @@ fun TVShowDTO.toTvShow(): TvShow {
         posterPath = "https://image.tmdb.org/t/p/w500${poster_path}",
         firstAirDate = first_air_date,
         rating = vote_average ?: 0.0
+    )
+}
+
+
+fun TVShowDetailDTO.toTvShowDetails(): TvShowDetails {
+    return TvShowDetails(
+        backdrop_path ="https://image.tmdb.org/t/p/w500${backdrop_path}" ,
+        poster_path = "https://image.tmdb.org/t/p/w500${poster_path}",
+        title = name,
+        tagline = tagline,
+        overview = overview,
+        genres = genres,
+        number_of_episodes = number_of_episodes,
+        vote_average = vote_average
     )
 }
